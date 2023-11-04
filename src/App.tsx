231eae39c488cfceb619be6a5e6bed536c39d7cf
Mateusz Dynur <mateusz.dynur@microsoft.wsei.edu.pl>
2023-11-04 20:34:51 +0100

    refactoring AppRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,23 @@
-import { useState, useEffect } from "react";
-import { getPosts } from "./services/usePosts";
-import { PostType } from "./types/PostType";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+    Route,
+} from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import NotFound from "./pages/NotFound";
+import Posts from "./pages/Posts";
 
 const App = () => {
-    const [posts, setPosts] = useState<PostType[]>([]);
-    const [isLoading, setLoading] = useState<boolean>(false);
-
-    const onSuccess = (data: PostType[]) => {
-        setPosts(data);
-    };
-
-    const onError = (data: unknown) => {
-        console.log(data);
-    };
-
-    useEffect(() => {
-        if (!isLoading) {
-            getPosts(onSuccess, onError);
-            setLoading(true);
-        }
-    }, [posts, isLoading]);
-
-    return (
-        <div className="bg-slate-800 w-full min-h-screen h-full p-6">
-            {!isLoading ? (
-                <div className="bg-slate-700 rounded-lg px-2 py-1 mb-2 ring-slate-900/5 shadow-lg text-white flex justify-center">Loading...</div>
-            ) : (
-                posts &&
-                posts.map((post) => (
-                    <div className="bg-slate-700 rounded-lg px-2 py-1 mb-2 ring-slate-900/5 shadow-lg" key={post.id}>
-                        <h1 className="text-white">{post.title}</h1>
-                        <p className="text-slate-400">{post.body}</p>
-                    </div>
-                ))
-            )}
-        </div>
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<AppLayout />}>
+                <Route path="/" element={<Posts />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>,
+        ),
     );
+    return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react";
+import { getPosts } from "../services/usePosts";
+import { PostType } from "../types/PostType";
+
 const Posts = () => {
+    const [posts, setPosts] = useState<PostType[]>([]);
+    const [isLoading, setLoading] = useState<boolean>(false);
+
+    const onSuccess = (data: PostType[]) => {
+        setPosts(data);
+    };
+
+    const onError = (data: unknown) => {
+        console.log(data);
+    };
+
+    useEffect(() => {
+        if (!isLoading) {
+            getPosts(onSuccess, onError);
+            setLoading(true);
+        }
+    }, [posts, isLoading]);
+
     return (
-        <div>
-            <h1>Posts</h1>
+        <div className="bg-slate-800 w-full min-h-screen h-full p-6">
+            {!isLoading ? (
+                <div className="bg-slate-700 rounded-lg px-2 py-1 mb-2 ring-slate-900/5 shadow-lg text-white flex justify-center">Loading...</div>
+            ) : (
+                posts &&
+                posts.map((post) => (
+                    <div className="bg-slate-700 rounded-lg px-2 py-1 mb-2 ring-slate-900/5 shadow-lg" key={post.id}>
+                        <h1 className="text-white">{post.title}</h1>
+                        <p className="text-slate-400">{post.body}</p>
+                    </div>
+                ))
+            )}
         </div>
     );
 };
