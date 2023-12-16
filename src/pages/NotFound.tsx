5cebd5fdeafdad89b963d1477d6a7aa43c7bf6d2
Mateusz Dynur <mateusz.dynur@microsoft.wsei.edu.pl>
2023-12-16 20:12:34 +0100

    change style NotFound Page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,6 +1,6 @@
 const NotFound = () => {
     return (
-        <div className="bg-slate-800 w-full h-screen p-6 flex justify-center items-center">
+        <div className="bg-slate-800 w-full h-full p-6 flex justify-center items-center">
             <div className="bg-slate-700  max-w-sm w-full rounded-lg px-4 py-2 ring-slate-900/5 shadow-lg">
                 <h1 className="text-white font-bold text-4xl flex justify-center mb-4">
                     404
